d2b3dc0e79bd5364b570ef1efa6ebfb855232c9d
Carlos Eduardo <cadumielkezim@gmail.com>
2022-03-31 22:02:24 -0300

    logos

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,26 @@
+<?php 
+
+return [
+
+    'authenticate_message' => 'This is a secure area of the application. Please confirm your password before continuing.',
+    'password' => 'Password',
+    'confirm' => 'Confirm',
+    'forgot_password_message' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+    'email' => 'Email',
+    'reset_password_message' => 'Email Password Reset Link',
+    'remember_me' => 'Remember me',
+    'create_account' => 'Create account',
+    'forgot_your_password' => 'Forgot your password',
+    'log_in' => 'Log in',
+    'name' => 'Name',
+    'confirm_password' => 'Confirm Password',
+    'already_registered' => 'Already registered?',
+    'register' => 'Register',
+    'reset_password' => 'Reset Password',
+    'email_verification_message' => 'Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.',
+    'new_link_message' => 'A new verification link has been sent to the email address you provided during registration.',
+    'resent_verification_email' => 'Resend Verification Email',
+    'log_out' => 'Log Out',
+]
+
+?>
---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,26 @@
+<?php 
+
+return [
+
+    'authenticate_message' => 'Esta é uma área segura do sistema. Por favor confirme sua senha para continuar.',
+    'password' => 'Senha',
+    'confirm' => 'Confirmar',
+    'forgot_password_message' => 'Esqueceu a sua senha? Sem problemas. Apenas nos informe seu email e enviaremos um link para resetar sua senha atual e escolher uma nova.',
+    'email' => 'Email',
+    'reset_password_message' => 'Link do Email Para Resetar Senha',
+    'remember_me' => 'Lembre-se de mim',
+    'create_account' => 'Criar conta',
+    'forgot_your_password' => 'Esqueceu sua senha?',
+    'log_in' => 'Entrar',
+    'name' => 'Nome',
+    'confirm_password' => 'Confirmar Senha',
+    'already_registered' => 'Já tem uma conta?',
+    'register' => 'Cadastrar',
+    'reset_password' => 'Resetar Senha',
+    'email_verification_message' => 'Obrigado por ter se cadastrado! Antes de começar, você poderia verificar seu endereço de email pelo link que te mandamos ? Se você não recebeu o email, enviaremos outro com prazer.',
+    'new_link_message' => 'Um novo link de verificação de email foi enviado para o seu endereço de email.',
+    'resent_verification_email' => 'Mandar email de verificação novamente',
+    'log_out' => 'Sair',
+]
+
+?>
