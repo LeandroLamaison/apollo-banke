4ede36d35a8a83bd336569306f788d6bb270ad8a
Leandro Lamaison <lamaisonleandro2@gmail.com>
2022-04-21 16:21:06 -0300

    🚸 Paint balance on balance card

--- resources/views/components/balance-card.blade.php
+++ resources/views/components/balance-card.blade.php
@@ -1,12 +1,19 @@
 @props(['balance'])
 
 <x-adminlte-card 
-    class="mt-5 text-xl text-center"
+    class="
+        mt-5 
+        text-xl 
+        text-center 
+        {{ $balance == 0 ? 'text-blue' : '' }}
+        {{ $balance > 0 ? 'text-green' : '' }}
+        {{ $balance < 0 ? 'text-red' : '' }}
+    "
     title="{{ __('account.available_balance') }}" 
     theme="lightblue" 
     theme-mode="outline"
     icon="fas fa-lg fa-solid fa-money-bill" 
     header-class="text-uppercase rounded-bottom border-info"
 >
-    {{ $balance }}
+    R$ {{ $balance }}
 </x-adminlte-card>
